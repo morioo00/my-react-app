4c8874363ddcb779aa34d163d3e376d97dc0faee
morioo00 <morita.kazuki.se@gmail.com>
2026-02-09 21:06:57 +0900

    update hello endpoint and react fetch

--- backend/src/main/java/HelloController.java
+++ backend/src/main/java/HelloController.java
@@ -2,11 +2,15 @@ package com.example.backend;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class HelloController {
+
     @GetMapping("/hello")
     public String hello() {
-        return "ok";
+        System.out.println("tarezou");
+        return "tare";
     }
 }
