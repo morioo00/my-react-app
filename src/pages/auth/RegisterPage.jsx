4afb1e6e9cd0a3ae35aa6eab1f0b24191f425631
morioo00 <morita.kazuki.se@gmail.com>
2026-02-13 18:40:49 +0900

    土台作り3

--- src/pages/auth/RegisterPage.jsx
+++ src/pages/auth/RegisterPage.jsx
@@ -1,28 +1,18 @@
-import AuthLayout from "./AuthLayout";
-import AuthCard from "./AuthCard";
-import AuthTitle from "./AuthTitle";
+import AuthLayout from "../../components/Login/AuthLayout";
+import AuthCard from "../../components/Login/AuthCard";
+import AuthTitle from "../../components/Login/AuthTitle";
 
 export default function RegisterPage() {
   return (
     <AuthLayout>
-      <div
-        style={{
-          width: "min(720px, 100%)",
-          display: "flex",
-          flexDirection: "column",
-          gap: 20,
-        }}
-      >
+      <AuthCard>
         <AuthTitle
           title="花谷APP"
-          subtitle="新規アカウント作成"
+          subtitle="～ 花谷の花谷による花谷の為のAPP ～"
         />
 
-        <AuthCard>
-          {/* RegisterFormがここに入る */}
-          <div style={{ height: 120 }} />
-        </AuthCard>
-      </div>
+        {/* ここに他チームの登録フォーム（テキストボックス等）を入れていく */}
+      </AuthCard>
     </AuthLayout>
   );
 }
