14d692ce8d6a9dd3957076b4537eb34a1a81a210
morioo00 <morita.kazuki.se@gmail.com>
2026-02-20 20:34:50 +0900

    H2 DB BE 作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./components/pages/LoginPage";
+import RegisterPage from "./components/pages/RegisterPage";
 
 export default function App() {
   return (
     <Routes>
       {/* 最初は/loginへ飛ばす */}
       <Route path="/" element={<Navigate to="/login" replace />} />
-
+      <Route path="/new-account" element={<RegisterPage />} />
       <Route path="/login" element={<LoginPage />} />
 
       {/* 404用 */}
